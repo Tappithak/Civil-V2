4b0f9e5cce9cb00c14a4dffad5c2bf01b59a1aa8
Tappithak <156490084+Tappithak@users.noreply.github.com>
2025-01-30 15:18:01 +0700

    fix icon web 2

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,9 +30,10 @@ export default function RootLayout({ children }) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#a20c0c96"/>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/icon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
       <head>
         <Script 
           src="https://code.highcharts.com/maps/highmaps.js"
