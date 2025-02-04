92f97f7d75a020c11a65efa7d02d2833559c028d
Tappithak <156490084+Tappithak@users.noreply.github.com>
2025-02-04 11:21:59 +0700

    แกไขกดย้อนกลับหลัง login แล้ว ไปเ้ขาสู่ระบบเหมือนเดิม

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,12 +1,37 @@
+'use client';
 import * as React from "react";
 import Login from "../../../components/auth/LoginForm";
 import Box from "@mui/material/Box";
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import Image from "next/image";
+import axios from "axios";
 
 
 
 const LoginPage = () => {
+
+  React.useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const response = await axios.get('/api/validate-token', {
+          withCredentials: true 
+        })
+        if (response.data.valid) {
+          // ถ้า Token ถูกต้อง อัพเดท User
+          window.location.href = '/pages/home';
+        }
+      } catch (error) {
+        // ถ้า Token ไม่ถูกต้อง ให้ลบ Token ออกจาก Cookies
+        //  window.location.href = '/auth/login';
+         console.log(error);
+      }
+    }
+    validateToken();
+
+
+  }, [])
+
+
   return (
     <Box className="flex justify-center items-center min-h-screen p-2 background-login">
       <Box className="w-[450px] h-[530px] border border-gray-200 m-auto p-10 rounded-[21px] color_bg shadow-xl z-10">
